8fb9c85ea93bf8c80e93c4dd739d7b14129b9ddd
yuenandi <17610160611@163.com>
2018-12-23 15:07:42 +0800

    new project

---
+++ spring-web/spring-web-style/src/main/java/com/castor/web/style/StyleApplication.java
@@ -0,0 +1,14 @@
+package com.castor.web.style;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyleApplication.class, args);
+    }
+
+}
+
